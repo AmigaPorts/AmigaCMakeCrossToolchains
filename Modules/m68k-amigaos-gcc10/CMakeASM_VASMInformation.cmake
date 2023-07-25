8f27bcc969c6a523220c7d2bf03f16ba704c947a
KaiN <kain@piwnica.ws>
2023-07-25 16:16:52 +0200

    generalize vasm cmake module

--- Modules/m68k-amigaos-gcc10/CMakeASM_VASMInformation.cmake
+++ Modules/m68k-amigaos-gcc10/CMakeASM_VASMInformation.cmake
@@ -1,10 +1,20 @@
-SET(ASM_DIALECT "_VASM")
-SET(CMAKE_ASM_VASM_SOURCE_FILE_EXTENSIONS vasm;asm)
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
 
-IF(NOT CMAKE_ASM_VASM_COMPILE_OBJECT)
-	SET(CMAKE_ASM_VASM_COMPILE_OBJECT "<CMAKE_ASM_VASM_COMPILER> <FLAGS> -o <OBJECT> <SOURCE>")
-ENDIF(NOT CMAKE_ASM_VASM_COMPILE_OBJECT)
+# Support for vasm assembler: m68k, ppc, et al.
 
-INCLUDE(CMakeASMInformation)
-SET(ASM_DIALECT)
+set(CMAKE_ASM_VASM_SOURCE_FILE_EXTENSIONS vasm;asm)
+
+# Allow overriding object format: ELF is usually the best option but e.g. m68k-amigaos might expect hunk
+if(NOT CMAKE_ASM_VASM_OBJECT_FORMAT)
+  set(CMAKE_ASM_VASM_OBJECT_FORMAT elf)
+endif()
+
+if(NOT CMAKE_ASM_VASM_COMPILE_OBJECT)
+  set(CMAKE_ASM_VASM_COMPILE_OBJECT "<CMAKE_ASM_VASM_COMPILER> <FLAGS> -F${CMAKE_ASM_VASM_OBJECT_FORMAT} -o <OBJECT> <SOURCE>" CACHE STRING "" FORCE)
+endif()
+
+set(ASM_DIALECT "_VASM")
+include(CMakeASMInformation)
+set(ASM_DIALECT)
 
--- Modules/m68k-amigaos-gcc10/CMakeTestASM_VASMCompiler.cmake
+++ Modules/m68k-amigaos-gcc10/CMakeTestASM_VASMCompiler.cmake
@@ -1,3 +1,12 @@
-SET(ASM_DIALECT "_VASM")
-INCLUDE(CMakeTestASMCompiler)
-SET(ASM_DIALECT)
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+# This file is used by EnableLanguage in cmGlobalGenerator to
+# determine that the selected ASM_VASM "compiler" works.
+# For assembler this can only check whether the compiler has been found,
+# because otherwise there would have to be a separate assembler source file
+# for each assembler on every architecture.
+
+set(ASM_DIALECT "_VASM")
+include(CMakeTestASMCompiler)
+set(ASM_DIALECT)
